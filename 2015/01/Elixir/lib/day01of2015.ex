a4b82ec70f5ba543151db984634ce2d559d4f576
Pablo Campana <pablocampana99@gmail.com>
2026-04-12 20:42:41 +0200

    Created elixir empty project

---
+++ 2015/01/Elixir/lib/day01of2015.ex
@@ -0,0 +1,18 @@
+defmodule Day01of2015 do
+  @moduledoc """
+  Documentation for `Day01of2015`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day01of2015.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2015/01/Elixir/test/day01of2015_test.exs
@@ -0,0 +1,8 @@
+defmodule Day01of2015Test do
+  use ExUnit.Case
+  doctest Day01of2015
+
+  test "greets the world" do
+    assert Day01of2015.hello() == :world
+  end
+end
